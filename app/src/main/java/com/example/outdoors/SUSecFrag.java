833faa4c5808bbb25f3fbde11461bc57c1d726ee
DaTsiiK <lazar.z@outlook.com>
2020-09-12 15:18:05 +0200

    sign up + login

---
+++ app/src/main/java/com/example/outdoors/SUSecFrag.java
@@ -0,0 +1,48 @@
+package com.example.outdoors;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SUSecFrag extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.signup_frag2, container,false);
+
+        Button prevButt = (Button) view.findViewById(R.id.prevbutton);
+
+        prevButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((SignUp)getActivity()).setViewPager(0);
+            }
+        });
+
+        Button submitButt = (Button) view.findViewById(R.id.signUpSubmit);
+
+        submitButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((SignUp)getActivity()).setInfo();
+            }
+        });
+
+        return view;
+    }
+
+    public String getInfo(int viewID){
+        EditText elET = (EditText) view.findViewById(viewID);
+        return elET.getText().toString().trim();
+    }
+}
